997e64eacecb346e7a0538c26175fe7d868fe1fd
Tristan De Ridder <131238058+TristanDeRidder@users.noreply.github.com>
2025-12-24 12:40:29 +0100

    evironment

--- MapItOut/src/canvasses/Continent.tsx
+++ MapItOut/src/canvasses/Continent.tsx
@@ -1,9 +1,53 @@
-import { useGLTF, OrbitControls, Clone } from "@react-three/drei";
+import { useGLTF, OrbitControls, Clone, Sky } from "@react-three/drei";
 import pinsData from "../data/pins.json";
 import CameraArcAnimation from "../components/Camera/ArcCamera";
-import { useState, useRef } from "react";
+import { useState, useRef, useMemo } from "react";
 import * as THREE from "three";
 import gsap from 'gsap';
+import { Water } from 'three-stdlib';
+import { extend, useThree, useFrame, useLoader } from '@react-three/fiber';
+
+extend({ Water });
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      water: any;
+    }
+  }
+}
+
+function Ocean() {
+  const ref = useRef<any>(null);
+  const gl = useThree((state) => state.gl);
+  const waterNormals = useLoader(
+  THREE.TextureLoader,
+  '/waternormals.jpeg'
+)
+
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), []);
+  const config = useMemo(
+    () => ({
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals,
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: false,
+      format: gl.outputColorSpace
+    }),
+    [waterNormals, gl.outputColorSpace]
+  );
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.material.uniforms.time.value += delta * 0.3;
+    }
+  });
+  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} position={[0, -1.5, 0]} />;
+}
 
 const Continent = () => {
   const [startArc, setStartArc] = useState(false);
@@ -30,6 +74,8 @@ const Continent = () => {
       ease: "power2.out",
     });
   };
+
+  
   return (
     <>
       {/* Controls */}
@@ -47,9 +93,13 @@ const Continent = () => {
         }}
       />
 
-      {/* Lights */}
+      {/* World */}
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1} />
+      <Ocean />
+      <group scale={1000}>
+        <Sky sunPosition={[500, 150, -1000]} turbidity={0.1} />
+      </group>
 
       {/* Models */}
       <primitive object={ContinentModel.scene} scale={0.02} position={[0, -1, 0]} />
