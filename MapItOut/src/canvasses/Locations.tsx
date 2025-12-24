ac2eb6333d33ce6a814b24196f53095d5554be34
Tristan De Ridder <131238058+TristanDeRidder@users.noreply.github.com>
2025-12-24 16:37:42 +0100

    Started world building detail pages

--- MapItOut/src/canvasses/Locations.tsx
+++ MapItOut/src/canvasses/Locations.tsx
@@ -1,4 +1,4 @@
-import { useGLTF, OrbitControls } from "@react-three/drei";
+import { useGLTF, OrbitControls, Sky } from "@react-three/drei";
 
 const Locations = ({ modelId }: { modelId?: string }) => {
     console.log("modelId:", modelId);
@@ -25,6 +25,9 @@ const Locations = ({ modelId }: { modelId?: string }) => {
       {/* Lights */}
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1} />
+      <group scale={1000}>
+        <Sky sunPosition={[500, 150, -1000]} turbidity={0.1} />
+      </group>
 
       {/* Models */}
       <primitive object={POIModel.scene} scale={0.02} position={[0, -1, 0]} />
