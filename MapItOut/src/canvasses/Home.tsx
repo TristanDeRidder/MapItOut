986c63dbe6ede7e3e3d2f5c86f0a30a034e75a37
Tristan De Ridder <131238058+TristanDeRidder@users.noreply.github.com>
2025-12-25 12:37:58 +0100

    Intro screen clouds

--- MapItOut/src/canvasses/Home.tsx
+++ MapItOut/src/canvasses/Home.tsx
@@ -1,47 +1,19 @@
-import { useGLTF, Sky, Text, PerspectiveCamera } from "@react-three/drei";
-import pinsData from "../data/pins.json";
-import { useRef, useMemo } from "react";
+import { useGLTF, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber"
 import { Clouds, Cloud, CameraControls, Sky as SkyImpl, StatsGl } from "@react-three/drei"
-import { useControls } from "leva"
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
 
 
 const SkyConfig = () => {
-  const ref = useRef<THREE.Group>(null)
-  const cloud0 = useRef<THREE.Group>(null)
-  const { color, x, y, z, range, ...config } = useControls({
-    seed: { value: 1, min: 1, max: 100, step: 1 },
-    segments: { value: 20, min: 1, max: 80, step: 1 },
-    volume: { value: 6, min: 0, max: 100, step: 0.1 },
-    opacity: { value: 0.8, min: 0, max: 1, step: 0.01 },
-    fade: { value: 10, min: 0, max: 400, step: 1 },
-    growth: { value: 4, min: 0, max: 20, step: 1 },
-    speed: { value: 0.1, min: 0, max: 1, step: 0.01 },
-    x: { value: 6, min: 0, max: 100, step: 1 },
-    y: { value: 1, min: 0, max: 100, step: 1 },
-    z: { value: 1, min: 0, max: 100, step: 1 },
-    range: { value: 100, min: 0, max: 500, step: 1 },
-    color: "white",
-  })
- 
   return (
     <>
       <SkyImpl />
-      <group ref={ref}>
-        <Clouds material={THREE.MeshLambertMaterial} limit={400} range={range}>
-          <Cloud ref={cloud0} {...config} bounds={[x, y, z]} color={color} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={5} position={[0, 0, 5]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={4} position={[0, 0, -5]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={3} position={[0, 0, 12]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={5} position={[0, 0, 5]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={4} position={[0, 0, -5]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={3} position={[0, 0, 12]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={5} position={[0, 0, 5]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={4} position={[0, 0, -5]} />
-          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={3} position={[0, 0, 12]} />
+      <group>
+        <Clouds material={THREE.MeshLambertMaterial} limit={400} range={100}>
+          <Cloud seed={4} segments={20} volume={6} opacity={0.8} fade={10} growth={4} speed={0.1} bounds={[6, 1, 1]} color="#c0c0dd" position={[-4, 0, -5]} />
+          <Cloud seed={3} segments={20} volume={6} opacity={0.8} fade={10} growth={5} speed={0.1} bounds={[6, 1, 1]} color="#c0c0dd" position={[6, 0, 5]} />
+          <Cloud seed={3} segments={20} volume={6} opacity={0.8} fade={10} growth={4} speed={0.1} bounds={[6, 1, 1]} color="#c0c0dd" position={[0, 0, 12]} />
         </Clouds>
       </group>
     </>
@@ -59,7 +31,7 @@ export const Home = () => {
       {/* Camera - Top View */}
       <PerspectiveCamera makeDefault position={[0, 15, 0]} rotation={[-Math.PI / 2, 0, 0]} />
   
-/* Post-Processing Effects */}
+      {/* Post-Processing Effects */}
       <EffectComposer>
         <Bloom
           intensity={1.2}
