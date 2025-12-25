bc925936c388bbd6bb5bcdf3b225a33deeada712
Tristan De Ridder <131238058+TristanDeRidder@users.noreply.github.com>
2025-12-25 11:06:22 +0100

    Better intro screen

--- MapItOut/src/canvasses/Home.tsx
+++ MapItOut/src/canvasses/Home.tsx
@@ -2,57 +2,56 @@ import { useGLTF, Sky, Text, PerspectiveCamera } from "@react-three/drei";
 import pinsData from "../data/pins.json";
 import { useRef, useMemo } from "react";
 import * as THREE from "three";
-import { Water } from 'three-stdlib';
-import { extend, useThree, useFrame, useLoader } from '@react-three/fiber';
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Clouds, Cloud, CameraControls, Sky as SkyImpl, StatsGl } from "@react-three/drei"
+import { useControls } from "leva"
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
 
-extend({ Water });
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      water: any;
-    }
-  }
-}
-
-function Ocean() {
-  const ref = useRef<any>(null);
-  const gl = useThree((state) => state.gl);
-  const waterNormals = useLoader(
-  THREE.TextureLoader,
-  '/waternormals.jpeg'
-)
-
-  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
-  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), []);
-  const config = useMemo(
-    () => ({
-      textureWidth: 512,
-      textureHeight: 512,
-      waterNormals,
-      sunDirection: new THREE.Vector3(),
-      sunColor: 0xffffff,
-      waterColor: 0x001e0f,
-      distortionScale: 3.7,
-      fog: false,
-      format: gl.outputColorSpace
-    }),
-    [waterNormals, gl.outputColorSpace]
-  );
-  useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.material.uniforms.time.value += delta * 0.1;
-    }
-  });
-  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} position={[0, -1.05, 0]} />;
-}
+const SkyConfig = () => {
+  const ref = useRef<THREE.Group>(null)
+  const cloud0 = useRef<THREE.Group>(null)
+  const { color, x, y, z, range, ...config } = useControls({
+    seed: { value: 1, min: 1, max: 100, step: 1 },
+    segments: { value: 20, min: 1, max: 80, step: 1 },
+    volume: { value: 6, min: 0, max: 100, step: 0.1 },
+    opacity: { value: 0.8, min: 0, max: 1, step: 0.01 },
+    fade: { value: 10, min: 0, max: 400, step: 1 },
+    growth: { value: 4, min: 0, max: 20, step: 1 },
+    speed: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    x: { value: 6, min: 0, max: 100, step: 1 },
+    y: { value: 1, min: 0, max: 100, step: 1 },
+    z: { value: 1, min: 0, max: 100, step: 1 },
+    range: { value: 100, min: 0, max: 500, step: 1 },
+    color: "white",
+  })
+ 
+  return (
+    <>
+      <SkyImpl />
+      <group ref={ref}>
+        <Clouds material={THREE.MeshLambertMaterial} limit={400} range={range}>
+          <Cloud ref={cloud0} {...config} bounds={[x, y, z]} color={color} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={5} position={[0, 0, 5]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={4} position={[0, 0, -5]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={3} position={[0, 0, 12]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={5} position={[0, 0, 5]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={4} position={[0, 0, -5]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={3} position={[0, 0, 12]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={5} position={[0, 0, 5]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={4} position={[0, 0, -5]} />
+          <Cloud {...config} bounds={[x, y, z]} color="#c0c0dd" seed={3} position={[0, 0, 12]} />
+        </Clouds>
+      </group>
+    </>
+  )}
 
 export const Home = () => {
   const ContinentModel = useGLTF(
     new URL("../models/AmaralysBaked2.glb", import.meta.url).href
   );
+
 
   
   return (
@@ -60,7 +59,7 @@ export const Home = () => {
       {/* Camera - Top View */}
       <PerspectiveCamera makeDefault position={[0, 15, 0]} rotation={[-Math.PI / 2, 0, 0]} />
   
-        {/* Post-Processing Effects */}
+/* Post-Processing Effects */}
       <EffectComposer>
         <Bloom
           intensity={1.2}
@@ -70,35 +69,15 @@ export const Home = () => {
   
       </EffectComposer>
 
-      {/* World */}
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
-      <ambientLight intensity={2} />
-      <Ocean />
-      <group scale={1000}>
-        <Sky sunPosition={[500, 150, -1000]} turbidity={0.1} />
-      </group>
-
-      {/* Models */}
-      <primitive object={ContinentModel.scene} scale={0.02} position={[0, -1, 0]} />
-
-      {pinsData.pins.map((pin, index) => (
-        <Text
-        key={index}
-        position={pin.position as [number, number, number]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        font="/fonts/Felipa-Regular.ttf"
-        fontSize={0.35}
-        color="rgba(255, 255, 255, 1)"
-        anchorX="center"
-        anchorY="middle"
-        outlineWidth={0.05}
-        outlineColor="rgba(255, 238, 0, 1) 0, 1)"
-        outlineOpacity={0.35}
-        material-toneMapped={false}
-      >
-        {pin.label}
-      </Text>
-      ))}
+      {/* Sky */}
+      <StatsGl />
+      <SkyConfig />
+      <ambientLight intensity={Math.PI / 1.5} />
+      <spotLight position={[0, 40, 0]} decay={0} distance={45} penumbra={1} intensity={100} />
+      <spotLight position={[-20, 0, 10]} color="red" angle={0.15} decay={0} penumbra={-1} intensity={30} />
+      <spotLight position={[20, -10, 10]} color="red" angle={0.2} decay={0} penumbra={-1} intensity={20} />
+      <CameraControls />
+            <primitive object={ContinentModel.scene} scale={0.02} position={[0, -1, 0]} />
 
 
     </>
