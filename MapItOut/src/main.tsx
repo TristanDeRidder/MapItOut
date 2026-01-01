b39fbf6887ef973645b3333888f5d1a1a8ba54c0
Tristan De Ridder <tristanderidder1@gmail.com>
2026-01-01 17:07:33 +0100

    Corrected URL

--- MapItOut/src/main.tsx
+++ MapItOut/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./pages/App.tsx";
 import POI from "./pages/POI.tsx";
-import Home from "./pages/Home.tsx";
 import {
   RouteLoaderOverlay,
   RouteLoaderProvider,
@@ -16,9 +15,8 @@ createRoot(document.getElementById("root")!).render(
       <RouteLoaderProvider>
         <RouteLoaderOverlay />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Amaralys" element={<App />} />
-          <Route path="/Amaralys/:modelId" element={<POI />} />
+          <Route path="/" element={<App />} />
+          <Route path="/:modelId" element={<POI />} />
         </Routes>
       </RouteLoaderProvider>
     </BrowserRouter>
